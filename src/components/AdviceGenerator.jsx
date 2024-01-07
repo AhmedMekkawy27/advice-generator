5726b8ca0be4d173804b9e6b9dd573129e757174
AhmedMekkawy27 <mekkawy39@gmail.com>
2024-01-07 07:37:55 +0200

    full code

---
+++ src/components/AdviceGenerator.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import divider from "../../public/pattern-divider-desktop.svg"
+import dice from "../../public/icon-dice.svg"
+export default function AdviceGenerator(){
+    let [randomNumber, setRandomNumber] = useState(62)
+    let [advices, setAdvices] = useState([])
+    useEffect(()=>{
+        fetch(`https://api.adviceslip.com/advice/${randomNumber}`)
+        .then(res=> res.json())
+        .then(json=>{
+            setAdvices(json)
+        })
+    },[randomNumber])
+    function randomGenerate(){
+        setRandomNumber((Math.random()*224).toFixed())
+    }
+    return(
+        <>
+            <div className="container">
+                <p className='advice-no'>Advice # {randomNumber}</p>
+                <h2 className='advice'>{advices.slip && JSON.stringify(advices.slip.advice)}</h2>
+                <div className="divider">
+                    <img src={divider} alt="" />
+                </div>
+                <button className='btn' onClick={randomGenerate}>
+                    <img src={dice} alt="" />
+                </button>
+            </div>
+        </>
+    )
+}
